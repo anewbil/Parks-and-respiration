f8b9b69120c0da7e598db80c9e3b28eaa170ef25
schneibley <Schneibley@gmail.com>
2024-06-20 16:29:33 -0400

    added id tag to searchbox and added Mike's js to main page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const searchButton = document.querySelector("#searchButton");
+  const searchInput = document.querySelector("#searchBox");
+  const parkCodeMap = {
+      'acadia': 'acad',
+      'yellowstone': 'yell',
+      'yosemite': 'yose',
+      'grand canyon': 'grca',
+      'adams': 'adam'
+  };
+  searchButton.addEventListener("click", function(event) {
+      event.preventDefault();
+      let parkName = searchInput.value.trim().toLowerCase();
+      if (parkName && parkCodeMap[parkName]) {
+          localStorage.setItem('parkName', parkName);
+          localStorage.setItem('parkCode', parkCodeMap[parkName]);
+         
+          window.location.href = 'index2.html';
+      } else {
+          alert('Please enter a valid park name.');
+      }
+  });
+});
+
+
 document.addEventListener('DOMContentLoaded', function () {
     
     var modalButtons = document.querySelectorAll('.modal-button');
